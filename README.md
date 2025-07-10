086f425955c8a882270d5f38c45998dad2008c0d
Marcos Aquino <marcos.aquino@unifesp.br>
2025-07-09 22:30:45 -0300

    feat: sidebar e readme

---
+++ README.md
@@ -0,0 +1,75 @@
+# 🏡 Monitoramento da Qualidade do Ar Interno - ESP32
+
+[![Python](https://img.shields.io/badge/python-3.9+-blue.svg)](https://www.python.org/downloads/)
+[![Streamlit](https://img.shields.io/badge/Streamlit-1.x-red.svg)](https://streamlit.io/)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Este projeto, parte da discplina Projeto em Engenharia da Computação 2025/1 da UNIFESP-ICT/SJC oferece uma interface intuitiva e em tempo real para monitorar a **qualidade do ar interno** utilizando dados coletados por um dispositivo ESP32. A aplicação web, desenvolvida com Streamlit, exibe leituras de diversos sensores, fornece referências teóricas sobre os parâmetros monitorados e permite receber alertas via e-mail.
+
+---
+
+## ✨ Funcionalidades
+
+* **Leitura de Sensores em Tempo Real**: Visualize a última leitura e a média da última hora para parâmetros como **Temperatura**, **Umidade**, **VOC** (Compostos Orgânicos Voláteis), **eCO₂** (Dióxido de Carbono Equivalente), **PM2.5** (Material Particulado 2.5) e **AQI** (Índice de Qualidade do Ar).
+* **Série Histórica**: Acompanhe a evolução dos dados de cada sensor ao longo do tempo através de gráficos interativos.
+* **Referência Teórica**: Acesse informações detalhadas sobre os sensores utilizados (AQI, CO2, PM2.5, Temperatura e Umidade) e tabelas de classificação para os níveis de AQI e eCO₂/CO₂ a partir do datasheet.
+* **Alertas Personalizados**: Cadastre seu e-mail para receber notificações quando os níveis dos sensores ultrapassarem os limites recomendados.
+* **Suporte**: Obtenha informações de contato para suporte e acesso direto ao repositório do projeto.
+
+---
+
+## 🚀 Como Executar
+
+Acesse o projeto através do link [] ou siga os passos abaixo para colocar o projeto em funcionamento na sua máquina local:
+
+### Pré-requisitos
+
+Certifique-se de ter o **Python 3.9** ou superior instalado. Você pode baixá-lo em [python.org](https://www.python.org/downloads/).
+
+### 1. Clonar o Repositório
+
+Primeiro, clone este repositório para o seu ambiente local:
+
+```bash
+git clone [https://github.com/gbeloso/esp32-environment.git](https://github.com/gbeloso/esp32-environment.git)
+cd esp32-environment
+```
+
+### 2. Configurar Variáveis de Ambiente
+Crie um arquivo .env na raiz do projeto e adicione a URL da sua API que fornece os dados do ESP32.
+
+```bash
+API_URL="SUA_URL_DA_API_AQUI"
+```
+
+Substitua "SUA_URL_DA_API_AQUI" pela URL real da sua API de dados.
+
+### 3. Instalar dependências
+
+Instale as bibliotecas Python necessárias usando pip a partir do arquivo requirements.txt
+```bash
+pip install -r requirements.txt
+```
+
+### 4. Executar a Aplicação Streamlit
+Após instalar as dependências, execute a aplicação Streamlit a partir do diretório raiz do projeto:
+
+```bash
+streamlit run app.py
+```
+
+### 🛠 Principais Tecnologias Utilizadas
+![Diagrama da arquitetura do sistema](esp32.png)
+
+- ESP32: microcontrolador com o código principal
+- Sensor ENS160 + AHT21: temperatura, umidade, VOC, CO2 e AQI
+- Sensor PM2.5 (PPD42NS): partículas PM2.5
+- MQTT: protocolo de envio dos dados
+- Thingspeak: para receber e armazenar os dados
+- Streamlit: Framework Python para criação de aplicações web interativas.
+- Requests: Biblioteca Python para fazer requisições HTTP a APIs.
+- Pandas: Biblioteca para manipulação e análise de dados.
+- re: Módulo para operações com expressões regulares.
+- datetime: Módulo para manipulação de datas e horas.
+- streamlit-autorefresh: Componente Streamlit para recarregar a página automaticamente.
+- python-dotenv: Para carregar variáveis de ambiente de um arquivo .env.
--- app.py
+++ app.py
@@ -11,8 +11,15 @@ load_dotenv()
 
 url = os.getenv("API_URL")
 
-
+st.set_page_config(layout="wide")
 st_autorefresh(interval=10 * 1000, key="refresh")
+st.sidebar.title("Menu")
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Selecione uma opção:",
+        ("🌡️ Leitura dos Sensores", "📚 Referência Teórica", "🔔 Alertas", "🛠 Suporte")
+    )
 
 def reg_filter (json_data, regex_pattern):
     filtered_data = {}
@@ -75,105 +82,221 @@ data_sample = feeds[(feeds['entry_id']<=feeds['entry_id'].max())&(feeds['entry_i
 
 fields = reg_filter(channel, r".*(field).*")
 
-st.set_page_config(layout="wide")
-st.title("🌍 Monitoramento da Qualidade do Ar - ESP32")
+st.title("🏡 Monitoramento da Qualidade do Ar Interno - ESP32")
 
 feeds['created_at'] = feeds.apply(convert_datetime, axis=1)
 
-st.header("Última leitura")
+if(add_radio == "🌡️ Leitura dos Sensores"):
 
-if data:
-    col1, col2, col3 = st.columns(3)
+    st.header("Última leitura")
 
-    with col1:
-        gerar_metric_texto_colorido("🌡️ Temperatura", last_data["field1"], "°C", 18, 30,
-                                    "Temperatura fora do ideal.")
-        gerar_metric_texto_colorido("💧 Umidade", last_data["field2"], "%", 30, 60,
-                                    "Umidade inadequada.")
+    if data:
+        col1, col2, col3 = st.columns(3)
 
-    with col2:
-        gerar_metric_texto_colorido("🧪 VOC", last_data["field3"], "ppm", 0, 500,
-                                    "Alta concentração de VOCs.")
-        gerar_metric_texto_colorido("🟢 eCO₂", last_data["field4"], "ppm", 400, 1000,
-                                    "CO₂ elevado.")
+        with col1:
+            gerar_metric_texto_colorido("🌡️ Temperatura", last_data["field1"], "°C", 18, 30,
+                                        "Temperatura fora do ideal.")
+            gerar_metric_texto_colorido("💧 Umidade", last_data["field2"], "%", 30, 60,
+                                        "Umidade inadequada.")
 
-    with col3:
-        gerar_metric_texto_colorido("🌫️ PM2.5", last_data["field5"], "µg/m³", 0, 35,
-                                    "Partículas finas elevadas.")
-        gerar_metric_texto_colorido("🏭 AQI", last_data["field6"], "", 4, 5,
-                                    "Qualidade do ar ruim.")
+        with col2:
+            gerar_metric_texto_colorido("🧪 VOC", last_data["field3"], "ppm", 0, 500,
+                                        "Alta concentração de VOCs.")
+            gerar_metric_texto_colorido("🟢 eCO₂", last_data["field4"], "ppm", 400, 1000,
+                                        "CO₂ elevado.")
 
-    timestamp = datetime.fromisoformat(last_data["created_at"])
-    st.caption(f"Última leitura: {timestamp.strftime('%d/%m/%Y %H:%M:%S')}")
-else:
-    st.warning("Nenhum dado recebido da API.")
+        with col3:
+            gerar_metric_texto_colorido("🌫️ PM2.5", last_data["field5"], "µg/m³", 0, 35,
+                                        "Partículas finas elevadas.")
+            gerar_metric_texto_colorido("🏭 AQI", last_data["field6"], "", 4, 5,
+                                        "Qualidade do ar ruim.")
 
-st.header("Média da última hora")
+        timestamp = datetime.fromisoformat(last_data["created_at"])
+        st.caption(f"Última leitura: {timestamp.strftime('%d/%m/%Y %H:%M:%S')}")
+    else:
+        st.warning("Nenhum dado recebido da API.")
 
-if data:
-    col1, col2, col3 = st.columns(3)
+    st.header("Média da última hora")
 
-    # Conversão
-    for f in ['field1', 'field2', 'field3', 'field4', 'field5', 'field6']:
-        data_sample[f] = data_sample[f].astype(float)
+    if data:
+        col1, col2, col3 = st.columns(3)
 
-    # Médias
-    temp = round(data_sample["field1"].mean(), 2)
-    umid = round(data_sample["field2"].mean(), 2)
-    voc = round(data_sample["field3"].mean(), 2)
-    eco2 = round(data_sample["field4"].mean(), 2)
-    pm25 = round(data_sample["field5"].mean(), 2)
-    aqi = round(data_sample["field6"].mean(), 2)
+        # Conversão
+        for f in ['field1', 'field2', 'field3', 'field4', 'field5', 'field6']:
+            data_sample[f] = data_sample[f].astype(float)
 
-    with col1:
-        gerar_metric_texto_colorido("🌡️ Temperatura", temp, "°C", 18, 30, "Temperatura fora do ideal.")
-        if temp < 18 or temp > 30:
-            alerta_visual("Alerta Temperatura", "Recomendação para temperatura fora do ideal na ultima hora", "#dc3545")
+        # Médias
+        temp = round(data_sample["field1"].mean(), 2)
+        umid = round(data_sample["field2"].mean(), 2)
+        voc = round(data_sample["field3"].mean(), 2)
+        eco2 = round(data_sample["field4"].mean(), 2)
+        pm25 = round(data_sample["field5"].mean(), 2)
+        aqi = round(data_sample["field6"].mean(), 2)
 
-        gerar_metric_texto_colorido("💧 Umidade", umid, "%", 30, 60, "Umidade inadequada.")
-        if umid < 30 or umid > 60:
-            alerta_visual("Alerta umidade", "Recomendação para umidade fora do ideal na ultima hora", "#dc3545")
+        with col1:
+           
+            gerar_metric_texto_colorido("🌡️ Temperatura", temp, "°C", 18, 30, "Temperatura fora do ideal.")
+            if temp < 18 or temp > 30:
+                alerta_visual("Alerta Temperatura", "Recomendação para temperatura fora do ideal na ultima hora", "#dc3545")
 
-    with col2:
-        gerar_metric_texto_colorido("🧪 VOC", voc, "ppm", 0, 500, "Alta concentração de VOCs.")
-        if voc > 500:
-            alerta_visual("Alerta VOCs", "Recomendação para VOCs fora do ideal na ultima hora", "#dc3545")
+            gerar_metric_texto_colorido("💧 Umidade", umid, "%", 30, 60, "Umidade inadequada.")
+            if umid < 30 or umid > 60:
+                alerta_visual("Alerta umidade", "Recomendação para umidade fora do ideal na ultima hora", "#dc3545")
 
-        gerar_metric_texto_colorido("🟢 eCO₂", eco2, "ppm", 400, 1000, "CO₂ elevado.")
-        if eco2 > 1000:
-            alerta_visual("Alerta eCO2", "Recomendação para eCO2 fora do ideal na ultima hora", "#dc3545")
+        with col2:
+            gerar_metric_texto_colorido("🧪 VOC", voc, "ppm", 0, 500, "Alta concentração de VOCs.")
+            if voc > 500:
+                alerta_visual("Alerta VOCs", "Recomendação para VOCs fora do ideal na ultima hora", "#dc3545")
 
-    with col3:
-        gerar_metric_texto_colorido("🌫️ PM2.5", pm25, "µg/m³", 0, 35, "Partículas finas elevadas.")
-        if pm25 > 35:
-            alerta_visual("Alerta PM2.5", "Recomendação para PM2.5 fora do ideal na ultima hora", "#dc3545")
+            gerar_metric_texto_colorido("🟢 eCO₂", eco2, "ppm", 400, 1000, "CO₂ elevado.")
+            if eco2 > 1000:
+                alerta_visual("Alerta eCO2", "Recomendação para eCO2 fora do ideal na ultima hora", "#dc3545")
 
-        gerar_metric_texto_colorido("🏭 AQI", aqi, "", 0, 50, "Qualidade do ar ruim.")
-        if aqi > 50:
-            alerta_visual("Alerta AQI", "Recomendação para AQI fora do ideal na ultima hora", "#dc3545")
+        with col3:
+            gerar_metric_texto_colorido("🌫️ PM2.5", pm25, "µg/m³", 0, 35, "Partículas finas elevadas.")
+            if pm25 > 35:
+                alerta_visual("Alerta PM2.5", "Recomendação para PM2.5 fora do ideal na ultima hora", "#dc3545")
 
-    timestamp = datetime.fromisoformat(last_data["created_at"])
-    st.caption(f"Última leitura: {timestamp.strftime('%d/%m/%Y %H:%M:%S')}")
-else:
-    st.warning("Nenhum dado recebido da API.")
+            gerar_metric_texto_colorido("🏭 AQI", aqi, "", 0, 50, "Qualidade do ar ruim.")
+            if aqi > 50:
+                alerta_visual("Alerta AQI", "Recomendação para AQI fora do ideal na ultima hora", "#dc3545")
 
-st.header("Série histórica")
+        timestamp = datetime.fromisoformat(last_data["created_at"])
+        st.caption(f"Última leitura: {timestamp.strftime('%d/%m/%Y %H:%M:%S')}")
+    else:
+        st.warning("Nenhum dado recebido da API.")
 
-fields_l = list(fields.items())
+    st.header("Série histórica")
 
-for i in range(0, len(fields), 2):
-    cols = st.columns(2)
-    for j, (field, field_name) in enumerate(fields_l[i:i+2]):
-        with cols[j]:
-            st.write(f"Serie histórica - {field_name}")
-            st.line_chart(feeds, 
-                    x='created_at', 
-                    y=field, 
-                    height=450, 
-                    width=600, 
-                    use_container_width=False,
-                    x_label='Tempo', 
-                    y_label=fields[field]
-                    )
-#st.json(channel)
-#st.write(data_sample.rename(columns=fields))
+    fields_l = list(fields.items())
+
+    for i in range(0, len(fields), 2):
+        cols = st.columns(2)
+        for j, (field, field_name) in enumerate(fields_l[i:i+2]):
+            with cols[j]:
+                st.write(f"Serie histórica - {field_name}")
+                st.line_chart(feeds, 
+                        x='created_at', 
+                        y=field, 
+                        height=450, 
+                        width=500, 
+                        use_container_width=False,
+                        x_label='Tempo', 
+                        y_label=fields[field]
+                        )
+    #st.json(channel)
+    #st.write(data_sample.rename(columns=fields))
+
+elif(add_radio == "📚 Referência Teórica"):
+    st.header("Referência Teórica dos Sensores")
+    st.write("Consulte aqui a documentação teórica dos sensores utilizados:")
+    st.write("1. **AQI e CO2**: Sensor ENS160")
+    st.write("Datasheet: https://www.mouser.com/datasheet/2/1081/SC_001224_DS_1_ENS160_Datasheet_Rev_0_95-2258311.pdf")
+
+    st.subheader("AQI - Índice de Qualidade do Ar")
+    tabela_aqi = {
+        '#': [1, 2, 3, 4, 5],
+        'Classificação': ['Excelente', 'Boa', 'Moderada', 'Ruim', 'Não Saudável'], 
+        'Classificação Higiênica': ['Sem objeções', 'Sem objeções relevantes', 'Algumas objeções', 'Maiores objeções', 'Situação não aceitável'], 
+        'Recomendação': ['Alvo', 'Ventilação suficiente recomendada', 'Ventilação aumentada recomendada', 'Ventilação intensificada recomendada', 'Usar apenas se inevitável: ventilação intensificada recomendada'],
+        'Limite de Exposição': ['sem limite', 'sem limite', '<12 meses', '<1 mês', 'horas'] 
+    }
+    df = pd.DataFrame(tabela_aqi)
+
+    color_map = {
+        'Excelente': 'background-color: #ADD8E6', # Azul Claro (aprox. "light blue")
+        'Boa': 'background-color: #90EE90',      # Verde Claro (aprox. "light green")
+        'Moderada': 'background-color: #FFD700',   # Dourado (aprox. "gold") - para amarelo
+        'Ruim': 'background-color: #FFA500',      # Laranja (aprox. "orange")
+        'Não Saudável': 'background-color: #FF6347'  # Tomate (aprox. "tomato") - um vermelho-alaranjado
+    }
+
+    text_color_map = {
+        'Excelente': 'color: black',
+        'Boa': 'color: black',
+        'Moderada': 'color: black',
+        'Ruim': 'color: black',
+        'Não Saudável': 'color: black'
+    }
+
+    def color_rows(row):
+        """Aplica a cor de fundo e de texto com base na coluna 'Rating'."""
+        rating = row['Classificação']
+        background_style = color_map.get(rating, '') 
+        text_style = text_color_map.get(rating, '')  
+
+        return [f'{background_style}; {text_style}' for _ in row.index]
+
+    st.dataframe(df.style.apply(color_rows, axis=1), hide_index=True)
+
+    st.info("Esta tabela fornece uma classificação da qualidade do ar com recomendações e limites de exposição correspondentes, de acordo com o datasheet do sensor.")
+
+#====
+    st.subheader("Tabela de Níveis de eCO2 / CO2 e Qualidade do Ar Interno")
+
+    data_co2 = {
+        'eCO₂ / CO₂': ['>1500', '1000 - 1500', '800 - 1000', '600 - 800', '400 - 600'],
+        'Classificação': ['Péssimo', 'Ruim', 'Razoável', 'Boa', 'Excelente'], # Tradução de Bad, Poor, Fair, Good, Excellent
+        'Comentário / Recomendação': [
+            'Ar interno fortemente contaminado / Ventilação necessária',
+            'Ar interno contaminado / Ventilação recomendada',
+            'Ventilação opcional',
+            'Média',
+            'Alvo'
+        ]
+    }
+    df = pd.DataFrame(data_co2)
+
+    color_map = {
+        'Péssimo': 'background-color: #FF6347',      # Tomate (para o vermelho de "Bad")
+        'Ruim': 'background-color: #FFA500',     # Laranja (para o laranja de "Poor")
+        'Razoável': 'background-color: #FFD700',  # Dourado (para o amarelo de "Fair")
+        'Boa': 'background-color: #90EE90',       # Verde Claro (para o verde de "Good")
+        'Excelente': 'background-color: #ADD8E6'  # Azul Claro (para o azul de "Excellent")
+    }
+
+    text_color_map = {
+        'Péssimo': 'color: white', 
+        'Ruim': 'color: black',
+        'Razoável': 'color: black',
+        'Boa': 'color: black',
+        'Excelente': 'color: black'
+    }
+
+    def colorir_linhas_co2(linha):
+        """Aplica a cor de fundo e de texto com base na coluna 'Classificação'."""
+        classificacao = linha['Classificação']
+        estilo_fundo = color_map.get(classificacao, '')
+        estilo_texto = text_color_map.get(classificacao, '')
+
+        return [f'{estilo_fundo}; {estilo_texto}' for _ in linha.index]
+
+    st.dataframe(df.style.apply(colorir_linhas_co2, axis=1), hide_index=True)
+
+    st.info("Esta tabela mostra os níveis de eCO₂/CO₂ e sua classificação, com comentários e recomendações sobre a qualidade do ar interno.")
+
+    st.write("2. **PM2.5**: Groove-Dust Sensor (PPD42NS)")
+    st.write("https://seeeddoc.github.io/Grove-Dust_Sensor/")
+
+    st.write("3. **Temperatura e Umidade**: Sensor AHT21")
+    st.write("Datasheet: https://mallimages.ofweek.com/Upload/guige/2025/03/20/6369/606bbf98f38ee.pdf")
+
+
+elif(add_radio == "🔔 Alertas"):
+    st.subheader("Alertas")
+    st.write("Adicione seu e-mail para receber notificações quando os níveis ultrapassarem os limites recomendados.")
+    email = st.text_input("Digite seu e-mail para receber alertas:", placeholder="example@gmail.com")
+    if st.button("Salvar e-mail"):
+        if email:
+            st.success(f"E-mail {email} salvo com sucesso!")
+        else:
+            st.error("Por favor, insira um e-mail válido.")
+    
+
+elif(add_radio == "🛠 Suporte"):
+    st.subheader("Suporte")
+    st.write("Em caso de dúvidas ou dificuldades, consulte nossa documentação no repositório: ")
+    st.markdown("[https://github.com/gbeloso/esp32-environment]")
+
+    st.write("Ou entre em contato com nosso suporte:")
+    st.write("Contato: ✉ example@email.com")
